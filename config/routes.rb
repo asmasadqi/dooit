9eb6aac9680bda3a4c99e0df85d62fe63611ef80
Kaori Martin <kaori.martin21@gmail.com>
2023-01-14 18:03:42 +0100

    routes updated but to revise

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,20 @@ Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  resources :activities do
+    resources :reviews, only: %i[new create]
+    resources :bookings, only: %i[new create]
+  end
+
+  resources :reviews, only: [:delete]
+
+  resources :users do
+    resources :activities, only: [:index]
+    resources :bookings, only: [:index]
+  end
+
+  resources :bookings, only: %i[edit update]
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
