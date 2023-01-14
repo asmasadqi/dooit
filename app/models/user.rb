725244c32e8f3aa36857e854064db765dfa4d86e
Kaori Martin <kaori.martin21@gmail.com>
2023-01-14 16:12:54 +0100

    created and migrated user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :bookings
+  has_many :activities
+  has_many :reviews, through: :bookings, dependent: :destroy
+
+  # validates :first_name, presence: true
+  # validates :last_name, presence: true
+  # validates :username, presence: true
+  # validates :email, presence: true
+  # validates :password, presence: true
 end
