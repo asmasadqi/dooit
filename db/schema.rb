dc4a17b59660b5c79fe6a9bba6c5f0bcb5b8729d
asma sadqi <asma.sadqi@gmail.com>
2023-01-14 14:43:52 +0100

    added model booking

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_14_150409) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_14_150500) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,6 +28,18 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_14_150409) do
     t.index ["user_id"], name: "index_activities_on_user_id"
   end
 
+  create_table "bookings", force: :cascade do |t|
+    t.datetime "time_start", precision: nil
+    t.datetime "time_end", precision: nil
+    t.decimal "total_price"
+    t.bigint "user_id", null: false
+    t.bigint "activity_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["activity_id"], name: "index_bookings_on_activity_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -41,4 +53,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_14_150409) do
   end
 
   add_foreign_key "activities", "users"
+  add_foreign_key "bookings", "activities"
+  add_foreign_key "bookings", "users"
 end
