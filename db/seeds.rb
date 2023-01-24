fe3d40c0a86d7121ed08cca3ad61b45d6c433a0a
ALexis Brun <alexisbn@outlook.com>
2023-01-25 00:04:00 +0100

    modification de la seed, navbar

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,13 +7,13 @@ Activity.destroy_all
 User.destroy_all
 puts "Database cleaned"
 
-puts 'Creating 10 fake users...'
+puts 'Creating 12 fake users...'
 
 users = []
 activities = []
 # categories = ["Art & Culture", "Entertainment", "Food & Drink", "Sports", "Tours", "Sightseeing", "Nature & Outdoors"]
 
-10.times do |i|
+12.times do |i|
   user = User.create(
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
@@ -28,8 +28,8 @@ end
 activity = Activity.create(
   user_id: users.sample.id,
   title: "Fishing",
-  description: "You'll be in our boat to go fishing in beautiful locations we have spotted. Catches of the day will be cooked for lunch by our wonderful chef",
-  category: "Nature & Outdoors",
+  description: "You'll be in our boat to go fishing in beautiful locations we have spotted. Catches of the day will be cooked for lunch by our wonderful chef.",
+  category: "Outdoors",
   localization: "Cassis",
   price_per_day: 75,
   image: Faker::LoremFlickr.image(size: "1000x500", search_terms: ['sports', 'fitness', 'art', 'food', 'entertainment', 'drink', 'sightseeing', 'nature'])
@@ -39,7 +39,7 @@ activity = Activity.create(
   user_id: users.sample.id,
   title: "Cooking",
   description: "Learn to cook amazing dishes with Laura, who owns many well-known restaurants in town.",
-  category: "Food & Drink",
+  category: "Food",
   localization: "Paris",
   price_per_day: 50,
   image: Faker::LoremFlickr.image(size: "1000x500", search_terms: ['sports', 'fitness', 'art', 'food', 'entertainment', 'drink', 'sightseeing', 'nature'])
@@ -48,7 +48,7 @@ activity = Activity.create(
 activity = Activity.create(
   user_id: users.sample.id,
   title: "Surf",
-  description: "Your local instructor will take you to the most beautiful beaches acording to your surfing skills and the day's condition",
+  description: "Your local instructor will take you to the most beautiful beaches acording to your surfing skills and the day's condition.",
   category: "Sports",
   localization: "Fuerteventura",
   price_per_day: 30,
@@ -58,7 +58,7 @@ activity = Activity.create(
 activity = Activity.create(
   user_id: users.sample.id,
   title: "Sightseeing in Rome",
-  description: "Discover Rome with our local guide. He will take you to touristic attractions such as: Colosseum, Roman Forum and many other places",
+  description: "Discover Rome with our local guide. He will take you to touristic attractions such as: Colosseum, Roman Forum and many other places.",
   category: "Sightseeing",
   localization: "Rome",
   price_per_day: 50,
@@ -68,7 +68,7 @@ activity = Activity.create(
 activity = Activity.create(
   user_id: users.sample.id,
   title: "Snowboard",
-  description: "Take our snowboard class this winter to level your skills up!.",
+  description: "Take our snowboard class this winter to level your skills up!",
   category: "Sports",
   localization: "Chamonix",
   price_per_day: 60,
@@ -78,8 +78,8 @@ activity = Activity.create(
 activity = Activity.create(
   user_id: users.sample.id,
   title: "Wine Tasting",
-  description: "Visit our winery in Hermitage with Thomas. He will take you to a tour to discover our wine production method. You will finish the visit with a wine tasting",
-  category: "Food & Drink",
+  description: "Visit our winery in Hermitage with Thomas. He will take you to a tour to discover our wine production method. You will finish the visit with a wine tasting.",
+  category: "Food",
   localization: "Lyon",
   price_per_day: 50,
   image: Faker::LoremFlickr.image(size: "1000x500", search_terms: ['sports', 'fitness', 'art', 'food', 'entertainment', 'drink', 'sightseeing', 'nature'])
@@ -108,8 +108,8 @@ activity = Activity.create(
 activity = Activity.create(
   user_id: users.sample.id,
   title: "Buggy Rental",
-  description: "Rent your own buggy for the day to take a nice tour in the desert of Namibia. Local guides will follow your ride to take you to breathtaking spots",
-  category: "Tour",
+  description: "Rent your own buggy for the day to take a nice tour in the desert of Namibia. Local guides will follow your ride to take you to breathtaking spots.",
+  category: "Tours",
   localization: "Namibia",
   price_per_day: 150,
   image: Faker::LoremFlickr.image(size: "1000x500", search_terms: ['sports', 'fitness', 'art', 'food', 'entertainment', 'drink', 'sightseeing', 'nature'])
@@ -119,7 +119,7 @@ activity = Activity.create(
   user_id: users.sample.id,
   title: "Pottery",
   description: "Make your own ceramic bowl from scratch, you will enjoy your meal everytime you fill you fill your own bowl with food.",
-  category: "Art & Culture",
+  category: "Culture",
   localization: "Berlin",
   price_per_day: 80,
   image: Faker::LoremFlickr.image(size: "1000x500", search_terms: ['sports', 'fitness', 'art', 'food', 'entertainment', 'drink', 'sightseeing', 'nature'])
@@ -128,7 +128,7 @@ activity = Activity.create(
 activity = Activity.create(
   user_id: users.sample.id,
   title: "Tour in Rio",
-  description: "You will be picked by our local guide with a van to take you to all the places you need to see in Rio de Janeiro",
+  description: "You will be picked by our local guide with a van to take you to all the places you need to see in Rio de Janeiro.",
   category: "Sightseeing",
   localization: "Rio de Janeiro",
   price_per_day: 100,
@@ -139,7 +139,7 @@ activity = Activity.create(
   user_id: users.sample.id,
   title: "Desert tour",
   description: "nice escape from the hustle and bustle of Marrakech , it is a great chance to savor a Moroccan berber tea and admire magical views , hop on a camel and enjoy the breathtaking views of the desert and scenery of the Atlas Mountains in background.",
-  category: "Tour",
+  category: "Tours",
   localization: "Marrakech",
   price_per_day: 130,
   image: Faker::LoremFlickr.image(size: "1000x500", search_terms: ['sports', 'fitness', 'art', 'food', 'entertainment', 'drink', 'sightseeing', 'nature'])
