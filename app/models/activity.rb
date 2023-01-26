05bd0c316af96eb32de310a164ee7d61e5bdd263
Kaori Martin <kaori.martin21@gmail.com>
2023-01-26 21:19:24 +0100

    removed has_one image

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,7 +2,6 @@ class Activity < ApplicationRecord
   belongs_to :user
   has_many :bookings
   has_many :reviews, through: :bookings
-  has_one_attached :image
 
   validates :title, presence: true
   validates :description, presence: true
