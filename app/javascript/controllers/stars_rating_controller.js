d420fba1e046cb7154ab73b4a3babb952370aa1d
Kaori Martin <kaori.martin21@gmail.com>
2023-01-26 21:34:37 +0100

    star_rating js deleted

--- app/javascript/controllers/stars_rating_controller.js
+++
@@ -1,8 +0,0 @@
-import { dynamicRating } from "../plugins/starsInReviewForm";
-const test = document.querySelector(".test")
-console.log("test")
-
-document.addEventListener('turbolinks:load', () => {
-  // [...]
-  dynamicRating();
-})
