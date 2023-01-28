606efe2564a98936bb945a7b7189197a2d05b500
Kaori Martin <kaori.martin21@gmail.com>
2023-01-28 15:37:02 +0100

    updated mine true

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,6 +1,6 @@
 class Activity < ApplicationRecord
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   has_many :reviews, through: :bookings
 
   validates :title, presence: true
